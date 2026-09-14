e792b91dfbf1a83c99ad58c2fcfdc58c2e588d52
Junbobd <jichenhua222@gmail.com>
2026-09-15 07:30:03 +0800

    登录校验功能

--- src/main/java/com/it_study/controller/LoginController.java
+++ src/main/java/com/it_study/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.it_study.controller;
 import com.it_study.pojo.Emp;
 import com.it_study.pojo.Result;
 import com.it_study.service.EmpService;
+import com.it_study.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -21,6 +25,20 @@ public class LoginController {
     public Result login(@RequestBody Emp emp){
         log.info("员工登录：{}",emp);
         Emp e = empService.login(emp);
-        return e!=null?Result.success():Result.error("用户名或密码错误");
+
+        //登陆成功，生成令牌，下发令牌
+        if(e!=null){
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",e.getId());
+            claims.put("name",e.getName());
+            claims.put("username",e.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+
+        //登陆失败，返回错误信息
+
+        return Result.error("用户名或密码错误");
     }
 }
--- src/test/java/com/it_study/TliasWebManagementApplicationTests.java
+++ src/test/java/com/it_study/TliasWebManagementApplicationTests.java
@@ -1,13 +1,43 @@
 package com.it_study;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//@SpringBootTest
 class TliasWebManagementApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGenJwt(){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id",1);
+        claims.put("name","Tom");
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "it_study")//签名算法
+                .setClaims(claims)//自定义内容（载荷）
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))//设置有效期为一个小时
+                .compact();
+
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void testParseJwt(){
+        Claims claims = Jwts.parser()
+                .setSigningKey("it_study")
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiVG9tIiwiaWQiOjEsImV4cCI6MTc4OTM5ODE3Mn0.HGOf-0phdKkC2wfuQYeN8xUi3dFGBidt7sV69BNNNe4")
+                .getBody();
+        System.out.println(claims);
     }
 
 }
