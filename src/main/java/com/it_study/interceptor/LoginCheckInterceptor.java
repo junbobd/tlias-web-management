be97a085f04b1f53443d7019495315a17754bd0d
Junbobd <jichenhua222@gmail.com>
2026-09-16 18:01:10 +0800

    登录校验功能-拦截器Interceptor

--- src/main/java/com/it_study/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/it_study/interceptor/LoginCheckInterceptor.java
@@ -1,27 +1,23 @@
-package com.it_study.filter;
-
-import ch.qos.logback.classic.spi.EventArgUtil;
-
+package com.it_study.interceptor;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.it_study.pojo.Result;
 import com.it_study.utils.JwtUtils;
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
-import java.io.IOException;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-//@WebFilter(urlPatterns = "/*")
-public class LoginCheckFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override//目标资源方法运行前运行。true放行，false不放行
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+
 
         //1.获取请求url
         String url = req.getRequestURI().toString();
@@ -30,8 +26,7 @@ public class LoginCheckFilter implements Filter {
         //2.判断请求url中是否包含token，如果包含，说明是登录操作，放行
         if(url.contains("login")){
             log.info("登陆操作，放行");
-            filterChain.doFilter(servletRequest,servletResponse);
-            return;
+            return true;
         }
 
         //3.获取请求头中的令牌（token）
@@ -44,7 +39,7 @@ public class LoginCheckFilter implements Filter {
             //手动转换 对象--json------------->阿里巴巴fastJSON
             String notLogin = JSONObject.toJSONString(error);
             resp.getWriter().write(notLogin);
-            return ;
+            return false;
         }
 
         //5.解析token，如果解析失败，返回错误结果（未登录）
@@ -57,10 +52,20 @@ public class LoginCheckFilter implements Filter {
             //手动转换 对象--json------------->阿里巴巴fastJSON
             String notLogin = JSONObject.toJSONString(error);
             resp.getWriter().write(notLogin);
-            return ;
+            return false;
         }
         //6.放行
         log.info("令牌合法，放行");
-        filterChain.doFilter(servletRequest,servletResponse);
+        return true;
+    }
+
+    @Override//目标资源方法运行后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle...");
+    }
+
+    @Override//视图渲染完毕后运行
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
     }
 }
